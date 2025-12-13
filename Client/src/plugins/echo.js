25982776cdb9187e5f44c58f8d67db7f112000f7
MahmoudReda <mahreda19@gmail.com>
2025-12-13 13:06:30 +0200

    gnrate all file docker using

--- Client/src/plugins/echo.js
+++ Client/src/plugins/echo.js
@@ -6,13 +6,14 @@ window.Pusher = Pusher;
 
 axios.defaults.withCredentials = true;
 axios.defaults.withXSRFToken = true;
+axios.defaults.baseURL = import.meta.env.VITE_AXIOS_BASE_URL; // Gets from docker-compose.yml environment
 
 
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsHost: import.meta.env.VITE_REVERB_HOST, // Gets from docker-compose.yml environment
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
